db7906430f2433ba77edf7393abe17878fc26e53
atadd <atadd@DESKTOP-PVF3S8C>
2024-01-24 17:47:29 +0100

    Aggiungere i file di progetto.

---
+++ Cane/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cane
+{
+    internal class Persona
+    {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+    }
+}
---
+++ Cane/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane cane = new Cane();  
+            Persona padrone=new Persona();
+            cane.nome = "Ringhio";
+            cane.razza = "Labrador";
+            cane.padrone.nome = "Simone";
+            Console.WriteLine("{0}",cane.padrone.nome);
+
+
+            Console.ReadLine();
+        }
+    }
+}
